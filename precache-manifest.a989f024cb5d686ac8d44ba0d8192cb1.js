0244e3543a382494895156cd13dd242548de6e3e
cyctw <yuaannn_192.cs06@g2.nctu.edu.tw>
2022-04-01 20:11:29 +0800

    Updates

---
+++ precache-manifest.a989f024cb5d686ac8d44ba0d8192cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71fa5b609e8921cb05c87e8c52a23db3",
+    "url": "/ImageLabel-DApp/index.html"
+  },
+  {
+    "revision": "d199fb4d57f631cd58a3",
+    "url": "/ImageLabel-DApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2c37727fa3d92b592c78",
+    "url": "/ImageLabel-DApp/static/js/2.9cf025fe.chunk.js"
+  },
+  {
+    "revision": "d199fb4d57f631cd58a3",
+    "url": "/ImageLabel-DApp/static/js/main.31d32268.chunk.js"
+  },
+  {
+    "revision": "f0485d0b758ddd3fdef0",
+    "url": "/ImageLabel-DApp/static/js/runtime-main.c0afe411.js"
+  }
+]);
