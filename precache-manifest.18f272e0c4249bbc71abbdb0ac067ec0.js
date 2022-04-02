ea65db0c86f4c686bd449d08b6bfe86746d29d73
CyCTW <43993825+CyCTW@users.noreply.github.com>
2022-04-02 13:59:36 +0000

    Deploying to gh-pages from @ CyCTW/ImageLabel-DApp@2174caad50bdd1c7dc7c9abf3b328c08004e8a37 🚀

---
+++ precache-manifest.18f272e0c4249bbc71abbdb0ac067ec0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3562990c0f3a735a2a1f8c0d3381f54",
+    "url": "/ImageLabel-DApp/index.html"
+  },
+  {
+    "revision": "65278887862bbd9c60b5",
+    "url": "/ImageLabel-DApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69cb64a0532cf151a2b0",
+    "url": "/ImageLabel-DApp/static/js/2.a5b78b3f.chunk.js"
+  },
+  {
+    "revision": "65278887862bbd9c60b5",
+    "url": "/ImageLabel-DApp/static/js/main.c6564dae.chunk.js"
+  },
+  {
+    "revision": "f0485d0b758ddd3fdef0",
+    "url": "/ImageLabel-DApp/static/js/runtime-main.c0afe411.js"
+  }
+]);
